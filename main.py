8cf8699f6c89c70fcfcf66e9ea285ae98b30110e
Marija <manchevaa06@gmail.com>
2025-12-13 17:06:42 +0100

    Add init_db and crypto.db for Render

--- main.py
+++ main.py
@@ -2,6 +2,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("crypto.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS coins (
+        id TEXT PRIMARY KEY,
+        name TEXT,
+        symbol TEXT,
+        market_cap REAL,
+        market_cap_rank INTEGER
+    )
+    """)
+    conn.commit()
+    conn.close()
+
+init_db()
+
 @app.get("/")
 def read_root():
     return {"message": "FastAPI is running successfully!"}
