f164656a7dda0fae2006146f9de73bc0a3d38aff
manchevamarija <146575764+manchevamarija@users.noreply.github.com>
2026-01-20 22:08:33 +0100

    Fix formatting in Crypto class table name declaration

--- domashna1/model/crypto.py
+++ domashna1/model/crypto.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Float
 from domashna1.data.crypto_db import Base
 
 class Crypto(Base):
-    __tablename__ = "cryptos"  # Make sure this matches your table name in crypto.db
+    __tablename__ = "cryptos"  
 
     id = Column(Integer, primary_key=True, index=True)
     symbol = Column(String, index=True)
